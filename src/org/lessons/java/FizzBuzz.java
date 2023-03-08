e2b2b0b08ad38f27472f72b00776c808d98a2516
manuelmenama <manuel.mena090589@gmail.com>
2023-03-08 17:03:34 +0100

    fizz buzz complete with bonus

--- src/org/lessons/java/FizzBuzz.java
+++ src/org/lessons/java/FizzBuzz.java
@@ -6,26 +6,34 @@ public class FizzBuzz {
     public static void main(String[] args) {
 
         int numberOfCycles;
-
+        boolean invalidInput = true;
         Scanner scan = new Scanner(System.in);
-        
-        System.out.print("Inserisci il numero di cicli: ");
-        numberOfCycles = Integer.parseInt(scan.nextLine());
 
-        scan.close();
+        while (invalidInput) {
 
-        for (int i = 0; i < numberOfCycles; i++) {
-            if ((i % 3 == 0) || (i % 5 == 0)) {
-                if (i % 3 == 0) {
-                    System.out.println("Fizz");
-                }
-                if (i % 5 == 0) {
-                    System.out.println("Buzz");
+            System.out.print("Inserisci il numero di cicli (tra 1 e 1000): ");
+            numberOfCycles = Integer.parseInt(scan.nextLine());
+
+            scan.close();
+            if (numberOfCycles > 0 && numberOfCycles <= 1000) {
+                invalidInput = false;
+                for (int i = 1; i <= numberOfCycles; i++) {
+                    if ((i % 3 == 0) || (i % 5 == 0)) {
+                        if (i % 3 == 0) {
+                            System.out.println("Fizz");
+                        }
+                        if (i % 5 == 0) {
+                            System.out.println("Buzz");
+                        }
+                    } else {
+                        System.out.println(i);
+                    }
                 }
             } else {
-                System.out.println(i);
+                System.out.println("*** Inserire un input valido ***");
             }
         }
+
 
     }
 }
