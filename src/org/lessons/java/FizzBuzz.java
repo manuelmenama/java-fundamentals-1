39e6c78674e0dfe5505b94fd2daef93f4f5a2628
manuelmenama <manuel.mena090589@gmail.com>
2023-03-08 16:37:13 +0100

    minimal fizz-buzz

--- src/org/lessons/java/FizzBuzz.java
+++ src/org/lessons/java/FizzBuzz.java
@@ -1,7 +1,31 @@
 package org.lessons.java;
 
+import java.util.Scanner;
+
 public class FizzBuzz {
     public static void main(String[] args) {
-        System.out.println("hello again java");
+
+        int numberOfCycles;
+
+        Scanner scan = new Scanner(System.in);
+        
+        System.out.print("Inserisci il numero di cicli: ");
+        numberOfCycles = Integer.parseInt(scan.nextLine());
+
+        scan.close();
+
+        for (int i = 0; i < numberOfCycles; i++) {
+            if ((i % 3 == 0) || (i % 5 == 0)) {
+                if (i % 3 == 0) {
+                    System.out.println("Fizz");
+                }
+                if (i % 5 == 0) {
+                    System.out.println("Buzz");
+                }
+            } else {
+                System.out.println(i);
+            }
+        }
+
     }
 }
