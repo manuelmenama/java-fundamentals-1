e42385f4ce098950662396119eea1ca081f33516
manuelmenama <manuel.mena090589@gmail.com>
2023-03-08 15:03:03 +0100

    first excercise done

--- src/org/lessons/java/TrainTicketCalculator.java
+++ src/org/lessons/java/TrainTicketCalculator.java
@@ -5,11 +5,14 @@ import java.util.Scanner;
 public class TrainTicketCalculator {
     public static void main(String[] args) {
         double pricePerKm = 0.21;
+        double youngDiscount = 0.2;
+        double silverDiscount = 0.4;
+
         int inputKm;
         int inputAge;
         Scanner scan = new Scanner(System.in);
 
-        System.out.print("Chilometri da percorrere: ");
+        System.out.print("Inserisci i chilometri da percorrere: ");
         inputKm = Integer.parseInt(scan.nextLine());
 
         System.out.println("*************");
@@ -18,8 +21,23 @@ public class TrainTicketCalculator {
         inputAge = Integer.parseInt(scan.nextLine());
 
         System.out.println("*************");
-        System.out.println(inputAge);
-        System.out.println(inputKm);
+
+        double totalPrice = (double) inputKm * pricePerKm;
+
+        System.out.println("Prezzo intero: " + totalPrice);
+
+        System.out.println("*************");
+
+        if (inputAge >= 65) {
+            totalPrice = totalPrice - totalPrice * silverDiscount;
+            System.out.print("Prezzo per gli over 65: " + totalPrice);
+        } else if (inputAge < 18) {
+            totalPrice = totalPrice - totalPrice * youngDiscount;
+            System.out.println("Prezzo per i minorenni: " + totalPrice);
+        } else {
+            System.out.println("Nessuno sconto applicato");
+        }
+
 
     }
 }
